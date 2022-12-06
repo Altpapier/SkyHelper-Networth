fa6dd3ae528d56f9eba7dd8336899df8c0110b9d
MattTheCuber <mattvine03@gmail.com>
2022-12-06 12:46:21 -0500

    feat: profile essence to sacks

--- calculators/sacksCalculator.js
+++ calculators/sacksCalculator.js
@@ -1,12 +1,13 @@
 const skyblockItems = require('../constants/items.json');
 const { validRunes } = require('../constants/misc');
+const { titleCase } = require('../helper/functions');
 
 const calculateSackItem = (item, prices) => {
   const itemPrice = prices[item.id.toLowerCase()] || 0;
   if (item.id.startsWith('RUNE_') && !validRunes.includes(item.id)) return null;
   if (itemPrice) {
     return {
-      name: skyblockItems.find((skyblockItem) => skyblockItem.id === item.id)?.name || 'Unknown',
+      name: item.name || skyblockItems.find((skyblockItem) => skyblockItem.id === item.id)?.name || titleCase(item.id),
       id: item.id,
       price: itemPrice * item.amount,
       calculation: [],
