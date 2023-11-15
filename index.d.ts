e923bd749af26e98ff2b847a10857140416021cc
Kath <55346310+Kathund@users.noreply.github.com>
2023-11-15 09:32:24 +0800

    feat(items): Networth infomation and calculations now show

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'skyhelper-networth' {
+declare module "skyhelper-networth" {
   export interface ItemData {
     name: string;
     count: number;
@@ -17,16 +17,92 @@ declare module 'skyhelper-networth' {
     museumData?: object;
   }
 
-  export interface NetworthResult {
-    networth: number;
-    items?: ItemData[];
+  export interface ItemNetworthOptions {
+    cache?: boolean;
+    prices?: object;
+    returnItemData?: boolean;
   }
 
-  export function getNetworth(profileData: object, bankBalance: number, options?: NetworthOptions): Promise<NetworthResult>;
+  export interface ItemCalculation {
+    id: string;
+    type: string;
+    price: number;
+    count: number;
+    star?: number;
+  }
 
-  export function getPreDecodedNetworth(profileData: object, items: object, bankBalance: number, options?: NetworthOptions): Promise<NetworthResult>;
+  export interface Item {
+    uuid?: string | null;
+    uniqueId?: string;
+    type?: string;
+    exp?: number;
+    active?: boolean;
+    teir?: string;
+    heldItem?: string | null;
+    candyUsed?: number;
+    skin?: string | null;
+    level?: number;
+    xpMax?: number;
+    name: string;
+    loreName?: string;
+    id: string;
+    price: number;
+    base: number;
+    calculation: ItemCalculation[];
+    count: number;
+    soulbound: boolean;
+  }
 
-  export function getItemNetworth(item: object, options?: NetworthOptions): Promise<{ networth: number; itemData?: ItemData }>;
+  export interface Category {
+    total: number;
+    unsoulboundTotal: number;
+    items?: Item[];
+  }
+
+  export interface Categories {
+    armor: Category;
+    equipment: Category;
+    wardrobe: Category;
+    inventory: Category;
+    enderchest: Category;
+    accessories: Category;
+    personal_vault: Category;
+    fishing_bag: Category;
+    potion_bag: Category;
+    candy_inventory: Category;
+    storage: Category;
+    museum: Category;
+    sacks: Category;
+    essence: Category;
+    pets: Category;
+  }
+
+  export interface NetworthResult {
+    noInventory: boolean;
+    networth: number;
+    unsoulboundNetworth: number;
+    purse: number;
+    bank: number;
+    types: Categories;
+  }
+
+  export function getNetworth(
+    profileData: object,
+    bankBalance: number,
+    options?: NetworthOptions
+  ): Promise<NetworthResult>;
+
+  export function getPreDecodedNetworth(
+    profileData: object,
+    items: object,
+    bankBalance: number,
+    options?: NetworthOptions
+  ): Promise<NetworthResult>;
+
+  export function getItemNetworth(
+    item: object,
+    options?: ItemNetworthOptions
+  ): Promise<Item>;
 
   export function getPrices(cache?: boolean): Promise<object>;
 }
