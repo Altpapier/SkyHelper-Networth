019db3629242e8d91012eab225b0083b9082c338
Altpapier <robin2leo@gmail.com>
2024-08-11 15:02:09 +0200

    add jsdoc

--- UpdateManager.js
+++ UpdateManager.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 
 class UpdateManager {
+    /**
+     * Creates an instance of UpdateManager and starts the interval for checking for updates (default: 1 minute).
+     * @returns {UpdateManager}
+     */
     constructor() {
         if (UpdateManager.instance) {
             return UpdateManager.instance;
@@ -12,11 +16,17 @@ class UpdateManager {
         this.intervalInstance = setInterval(this.checkForUpdate, this.interval);
     }
 
+    /**
+     * Disables the interval for checking for updates.
+     */
     disable() {
         clearInterval(this.intervalInstance);
         this.intervalInstance = null;
     }
 
+    /**
+     * Enables the interval for checking for updates. Only useful if the interval is disabled.
+     */
     enable() {
         if (this.intervalInstance) {
             return;
@@ -25,12 +35,19 @@ class UpdateManager {
         this.intervalInstance = setInterval(this.checkForUpdate, this.interval);
     }
 
+    /**
+     * Changes the interval for checking for updates.
+     * @param {Number} interval The interval in milliseconds to check for updates.
+     */
     setInterval(interval) {
         this.interval = interval;
         clearInterval(this.intervalInstance);
         this.intervalInstance = setInterval(this.checkForUpdate, this.interval);
     }
 
+    /**
+     * Checks for updates of the package on npm.
+     */
     async checkForUpdate() {
         try {
             const packageInfo = await axios.get('https://registry.npmjs.org/skyhelper-networth');
