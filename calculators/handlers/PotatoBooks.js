441e3407e2996b810e1a167891411c50e52c90ff
Matthew Vine <mattvine03@gmail.com>
2025-02-23 14:32:04 -0500

    test(handlers): add potato book handler tests

--- calculators/handlers/PotatoBooks.js
+++ calculators/handlers/PotatoBooks.js
@@ -19,9 +19,18 @@ class HotPotatoBookHandler {
      * @param {object} prices A prices object generated from the getPrices function
      */
     calculate(item, prices) {
-        const hotPotatoCount = Number(item.itemData.tag.ExtraAttributes.hot_potato_count);
-        if (hotPotatoCount > 10) {
-            const fumingPotatoBookCount = hotPotatoCount - 10;
+        const potatoBookCount = Number(item.itemData.tag.ExtraAttributes.hot_potato_count);
+        const hotPotatoBookCount = Math.min(potatoBookCount, 10);
+
+        const calculationData = {
+            id: 'HOT_POTATO_BOOK',
+            type: 'HOT_POTATO_BOOK',
+            price: (prices['HOT_POTATO_BOOK'] ?? 0) * hotPotatoBookCount * APPLICATION_WORTH.hotPotatoBook,
+            count: hotPotatoBookCount,
+        };
+
+        if (potatoBookCount > 10) {
+            const fumingPotatoBookCount = potatoBookCount - 10;
 
             const calculationData = {
                 id: 'FUMING_POTATO_BOOK',
@@ -33,14 +42,6 @@ class HotPotatoBookHandler {
             item.price += calculationData.price;
             item.calculation.push(calculationData);
         }
-
-        const hotPotatoBookCount = Math.min(hotPotatoCount, 10);
-        const calculationData = {
-            id: 'HOT_POTATO_BOOK',
-            type: 'HOT_POTATO_BOOK',
-            price: (prices['HOT_POTATO_BOOK'] ?? 0) * hotPotatoBookCount * APPLICATION_WORTH.hotPotatoBook,
-            count: hotPotatoBookCount,
-        };
 
         item.price += calculationData.price;
         item.calculation.push(calculationData);
