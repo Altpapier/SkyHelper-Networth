50648ee653030f1591449c72bd7d3fff276378ef
Matthew Vine <mattvine03@gmail.com>
2025-03-02 08:28:32 -0500

    test: remove unused arg

--- tests/handlers/BaseHandlerTest.js
+++ tests/handlers/BaseHandlerTest.js
@@ -8,48 +8,30 @@ class BaseHandlerTest {
 
     runTests() {
         describe(`${this.HandlerClass.name} Tests`, () => {
-            this.testCases.forEach(
-                ({
-                    description,
-                    item,
-                    prices,
-                    shouldApply,
-                    expectedNewPrice = undefined,
-                    expectedNewBasePrice = undefined,
-                    expectedPriceChange = 0,
-                    expectedCalculation = [],
-                }) => {
-                    it(description, () => {
-                        const handler = new this.HandlerClass();
+            this.testCases.forEach(({ description, item, prices, shouldApply, expectedNewBasePrice = undefined, expectedPriceChange = 0, expectedCalculation = [] }) => {
+                it(description, () => {
+                    const handler = new this.HandlerClass();
 
-                        // Check if handler applies
-                        const applies = handler.applies(item);
-                        assert.strictEqual(applies, shouldApply, `Expected applies() to return ${shouldApply}`);
+                    // Check if handler applies
+                    const applies = handler.applies(item);
+                    assert.strictEqual(applies, shouldApply, `Expected applies() to return ${shouldApply}`);
 
-                        // Validate calculation
-                        const priceBefore = item.price;
-                        if (shouldApply) {
-                            handler.calculate(item, prices);
-                        }
-                        assert.deepStrictEqual(item.calculation, expectedCalculation, 'Calculation does not match expected');
-                        if (expectedNewBasePrice !== undefined) {
-                            assert.strictEqual(
-                                item.basePrice,
-                                expectedNewBasePrice,
-                                `Expected base price to be set to ${expectedNewBasePrice} but got ${item.basePrice}`,
-                            );
-                        }
-                        if (expectedNewPrice !== undefined) {
-                            assert.strictEqual(item.price, expectedNewPrice, `Expected price to be set to ${expectedNewPrice} but got ${item.price}`);
-                        }
-                        assert.strictEqual(
-                            item.price - priceBefore,
-                            expectedPriceChange,
-                            `Expected price to increase by ${expectedPriceChange} but got ${item.price - priceBefore}`,
-                        );
-                    });
-                },
-            );
+                    // Validate calculation
+                    const priceBefore = item.price;
+                    if (shouldApply) {
+                        handler.calculate(item, prices);
+                    }
+                    assert.deepStrictEqual(item.calculation, expectedCalculation, 'Calculation does not match expected');
+                    if (expectedNewBasePrice !== undefined) {
+                        assert.strictEqual(item.basePrice, expectedNewBasePrice, `Expected base price to be set to ${expectedNewBasePrice} but got ${item.basePrice}`);
+                    }
+                    assert.strictEqual(
+                        item.price - priceBefore,
+                        expectedPriceChange,
+                        `Expected price to increase by ${expectedPriceChange} but got ${item.price - priceBefore}`,
+                    );
+                });
+            });
         });
     }
 }
