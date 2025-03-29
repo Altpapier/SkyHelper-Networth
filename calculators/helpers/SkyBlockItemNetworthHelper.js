df046ca9a00537c678435787ea0caad14c336861
Matthew Vine <mattvine03@gmail.com>
2025-03-29 18:27:14 -0400

    fix: use base id for cosmetic item checks

--- calculators/helpers/SkyBlockItemNetworthHelper.js
+++ calculators/helpers/SkyBlockItemNetworthHelper.js
@@ -161,7 +161,7 @@ class SkyBlockItemNetworthHelper {
         const testId = (this.itemId + this.itemName).toUpperCase();
         const isSkinOrDye = testId.includes('DYE') || testId.includes('SKIN');
         const isCosmetic = this.skyblockItem.category === 'COSMETIC' || this.itemLore.at(-1)?.includes('COSMETIC');
-        const isOnCosmeticBlacklist = NON_COSMETIC_ITEMS.has(this.itemId);
+        const isOnCosmeticBlacklist = NON_COSMETIC_ITEMS.has(this.baseItemId);
 
         return isCosmetic || isSkinOrDye || isOnCosmeticBlacklist || this.isRune() || this.isUniqueRune();
     }
