df077ae17e40c8870eadeddfd0acd64ef2c1993d
Matthew Vine <mattvine03@gmail.com>
2025-09-30 18:47:16 -0400

    test(handlers): add bigint test for crown of avarice coins collected handler

--- tests/handlers/AvariceCoinsCollected.test.js
+++ tests/handlers/AvariceCoinsCollected.test.js
@@ -66,6 +66,27 @@ const testCases = [
         ],
     },
     {
+        description: 'Applies correctly BigInt',
+        item: {
+            itemId: 'CROWN_OF_AVARICE',
+            extraAttributes: { collected_coins: BigInt(1_000_000_000_000) },
+            basePrice: 100,
+            price: 100,
+            calculation: [],
+        },
+        prices: { CROWN_OF_AVARICE: 250_000_000, CROWN_OF_AVARICE_1B: 4_500_000_000 },
+        shouldApply: true,
+        expectedNewBasePrice: 4_500_000_000,
+        expectedCalculation: [
+            {
+                id: 'CROWN_OF_AVARICE',
+                type: 'CROWN_OF_AVARICE',
+                price: 4_500_000_000,
+                count: 1_000_000_000,
+            },
+        ],
+    },
+    {
         description: 'Does not apply',
         item: {
             itemId: 'IRON_SWORD',
